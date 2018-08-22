bedd967fc4334f26d46f1986e17c33d75170e39f
Natasha Cox <natashac1014@gmail.com>
2018-08-22 11:04:39 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listsPlanets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            List<string> lastPlanets = new List<string>() { "Uranus", "Neptune"};
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            planetList.AddRange( lastPlanets );
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            
+
+            foreach (string item in planetList)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (string item in rockyPlanets)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
